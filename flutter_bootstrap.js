ecd4e68bbec0c5d82b79880c3917101cc4947df0
Roberto <robworkhub@gmail.com>
2025-07-21 14:04:15 +0200

    Aggiunta animazione

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3149441923"
+    serviceWorkerVersion: "1106250944"
   }
 });
